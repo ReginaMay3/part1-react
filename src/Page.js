8b5b29c8f56659b0425bba6f118bfef5e543b25a
ReginaMay3 <reginaalcmay@gmail.com>
2025-11-06 19:35:34 -0500

    Refactor Page component: update structure and styles for improved layout and readability

--- src/Page.js
+++ src/Page.js
@@ -1,17 +1,19 @@
 const Page = () => {
   return (
-    <>
-    <h2 class="bg-gradient-to-br from-purple-500 to-purple-200 bg-clip-text text-transparent">Ingeniera en Mecatrónica</h2>
-    <br/>
-    <img class="h-52 md:ml-12 pt-4; w-auto rounded-full m-auto" src="./images/perfil.jpg" alt="Regina May" />
-    <br/>
-    <h2 class="bg-gradient-to-b from-purple-500 to-purple-200 bg-clip-text text-transparent">Apasionada por el Desarrollo Web</h2>
-    <br/>
-    <div class="bg-[rgba(98,121,187,0.4)] md:w-72 h auto; w-auto h-auto text-justify z-50 p-2.5 rounded-xl">
-      <p class="text-white/90">Actualmente estoy en busca de mi primera experiencia laboral como desarrolladora Front-end. Me encanta aprender y enfrentar nuevos retos cada día.</p>
-      <p class="text-white/90">Estoy emocionada por comenzar mi carrera en el desarrollo web y contribuir con mis habilidades y entusiasmo a un equipo dinámico.</p>
+    <div>
+      <div class="bg-blue-800/30 md:w-72 h auto; w-auto h-auto text-center z-50 p-2.5 rounded-xl mb-4">
+        <h2 class="bg-gradient-to-b from-blue-500 to-purple-200 bg-clip-text text-transparent">Ingeniera en Mecatrónica</h2>
+        <br/>
+        <img class="h-52 md:ml-12 pt-4; w-auto rounded-full m-auto" src="./images/perfil.jpg" alt="Regina May" />
+        <br/>
+        <h2 class="bg-gradient-to-b from-blue-500 to-purple-100 bg-clip-text text-transparent">Apasionada por el Desarrollo Web</h2>
+        <br/>
+      </div>
+      <div class="bg-blue-800/30 md:w-72 h auto; w-auto h-auto text-justify z-50 p-2.5 rounded-xl">
+        <p class="text-white/90">Actualmente estoy en busca de mi primera experiencia laboral como desarrolladora Front-end. Me encanta aprender y enfrentar nuevos retos cada día.</p>
+        <p class="text-white/90">Estoy emocionada por comenzar mi carrera en el desarrollo web y contribuir con mis habilidades y entusiasmo a un equipo dinámico.</p>
+      </div>
     </div>
-    </>
     
     );
 }
